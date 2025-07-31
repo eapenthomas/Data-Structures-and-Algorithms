745df6dc216ce9d2de054ce94bd8041b2afa0858
eapenth <eapentkadamapuzha@gmail.com>
2025-07-31 06:29:57 +0530

    group anagrams

--- String/group anagrams.py
+++ String/group anagrams.py
@@ -3,15 +3,17 @@ from typing import List
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams_dict = defaultdict(list)
+        # letters nte count kittan vendi ..like c ondel 3 rd positionil 1 varum
         for s in strs:
-            count = [0]*26
-        # updating the  position when the letter is found based on ascii
+            count = [0] * 26
             for c in s:
                 count[ord(c) - ord('a')] += 1
-        # creating the key for the dictionary
+        #python dictionary needs immutable keys.. list is mutable so we convert the count into tuple
             key = tuple(count)
+        # why appending rather than =  is that ipo equal to anel last word mathrame keru like ate,nat oke ...full list kittan anu append() use cheyyune
             anagrams_dict[key].append(s)
-        return list(anagrams_dict.values())
+
+        return list(anagrams_dict.values() )
 
 strs =["eat","tea","tan","ate","nat","bat"]
 sol =  Solution()
